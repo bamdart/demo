b45919a103f4e312a9e1481aac55d69ba329e8a6
bamdart <bamdart@gmail.com>
2015-12-07 20:27:52 +0800

    2.4

--- path index.py
+++ path index.py
@@ -1,11 +1,25 @@
-from bottle import run, route,template
+from bottle import run, route, template, static_file,view
 
 @route('/')
+@view ('static/index.html')
 def index():
-        return template('static/index.html')
+	post = [
+		{
+			'author':'bamdart'
+			'message':'Hello,bottle.py'
+		}
+	]
+
+	for i in range (10):
+		posts.append({
+			'author': 'User #%d' % (i + 100),
+			'message': 'I Love Python. ' * (i + 1)
+			})
+	return {'posts':posts}
+
 
 @route('/static/<filename:path>')
 def static(filename):
-        return static_file(filename,root = 'static')
-        
+	return static_file(filename, root='static')
+
 run()
